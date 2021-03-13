f0d5f392397bb770dfa32729d07722b4eab68878
Hoang Nong <hoang.nong.it@gmail.com>
2021-03-13 09:44:35 +0000

    Update color

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -59,17 +59,17 @@ export function colors(darkMode: boolean): Colors {
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#F0B90B' : '#F0B90B',
+    primary1: darkMode ? '#5aa62b' : '#5aa62b',
     primary2: darkMode ? '#3680E7' : '#FF8CC3',
     primary3: darkMode ? '#4D8FEA' : '#FF99C9',
     primary4: darkMode ? '#376bad70' : '#F6DDE8',
-    primary5: darkMode ? '#795e08' : '#FDEAF1',
+    primary5: darkMode ? '#5aa62b' : '#FDEAF1',
 
     // color text
-    primaryText1: darkMode ? '#ffffff' : '#F0B90B',
+    primaryText1: darkMode ? '#ffffff' : '#5aa62b',
 
     // secondary colors
-    secondary1: darkMode ? '#F0B90B' : '#F0B90B',
+    secondary1: darkMode ? '#5aa62b' : '#5aa62b',
     secondary2: darkMode ? '#17000b26' : '#F6DDE8',
     secondary3: darkMode ? '#17000b26' : '#FDEAF1',
 
@@ -79,7 +79,7 @@ export function colors(darkMode: boolean): Colors {
     green1: '#27AE60',
     yellow1: '#FFE270',
     yellow2: '#F3841E',
-    blue1: '#F0B90B'
+    blue1: '#5aa62b'
 
     // dont wanna forget these blue yet
     // blue4: darkMode ? '#153d6f70' : '#C4D9F8',
